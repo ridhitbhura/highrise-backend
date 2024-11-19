bf991c6d533dc5374360a32d96e4d0b61affe0cd
ridhitbhura <ridhitbhura1234@gmail.com>
2024-11-18 20:21:42 -0800

    Added start script

--- app/main.py
+++ app/main.py
@@ -26,7 +26,3 @@ app.include_router(router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Highrise FAQ Chatbot API!"}
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", default=8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
