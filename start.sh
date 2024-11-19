f5b42cab39363117d8972bfa91ac74937ff21174
ridhitbhura <ridhitbhura1234@gmail.com>
2024-11-18 21:48:29 -0800

    Added background worker for port fix and testing ui

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-PORT="${PORT:-8000}"
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+# Use PORT from environment variable if set, otherwise default to 8000
+export PORT="${PORT:-8000}"
+uvicorn app.main:app --host 0.0.0.0 --port $PORT --workers 1
