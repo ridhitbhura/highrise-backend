2308ade7cc4a63a747c1775a055b17219ca11102
ridhitbhura <ridhitbhura1234@gmail.com>
2024-11-18 19:44:05 -0800

    added port binding

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import router
+import uvicorn
+import os
 
 # Initialize the app
 app = FastAPI(
@@ -14,3 +16,7 @@ app.include_router(router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Highrise FAQ Chatbot API!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", default=8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
